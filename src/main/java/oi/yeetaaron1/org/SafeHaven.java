cce60e5d53c8d55a62cc25682fb43c3f50ecc940
Aaron <cancholaron@gmail.com>
2024-08-27 19:02:05 -0700

    0.01-ALPHA

---
+++ src/main/java/oi/yeetaaron1/org/SafeHaven.java
@@ -0,0 +1,17 @@
+package oi.yeetaaron1.org;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SafeHaven extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
